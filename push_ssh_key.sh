c5e7c1b413db98560b3291cb9e50070ab03b3f55
josuebrunel <josuebrunel@gmail.com>
2015-02-25 10:12:59 -0600

    push_ssh_key now uses exit_f

--- push_ssh_key.sh
+++ push_ssh_key.sh
@@ -4,7 +4,7 @@
 #   Filename        : push_ssh_key.sh
 #   Description     : Copy a ssh key to a remote host
 #   Creation Date   : 25-02-2015
-#   Last Modified   : Wed 25 Feb 2015 10:00:36 AM CST
+#   Last Modified   : Wed 25 Feb 2015 10:11:51 AM CST
 #
 ##################################################
 
@@ -12,7 +12,7 @@ function push_ssh_key(){
     pub_key="$HOME/.ssh/id_rsa.pub"
     if [ ! -f $pub_key ]; then
         echo -e "I think you should generate a pub_key first. \n \t -> ssh-keygen # if you have no private key \n \t -> ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub"
-        return 1
+        exit_f
     fi
 
     if [ ! -z $1 ]; then
@@ -20,6 +20,6 @@ function push_ssh_key(){
         return 0
     else
         echo "You must provided a remote host i.e john@doe"
-        return 1
+        exit_f
     fi
 }
