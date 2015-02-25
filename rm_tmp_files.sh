ab87791c7214d62749dff033b43ccdaed3aa04c0
josuebrunel <josuebrunel@gmail.com>
2015-02-25 10:27:53 -0600

    exit_f now used by rm_tmp_files

--- rm_tmp_files.sh
+++ rm_tmp_files.sh
@@ -1,18 +1,16 @@
-#!/bin/bash
-
 ##################################################
 #
 #   Author          : yosuke
 #   Filename        : rm_tmp_files.sh
 #   Description     : Remove ~, .pyc recursively 
 #   Creation Date   : 25-02-2015
-#   Last Modified   : Wed 25 Feb 2015 09:25:59 AM CST
+#   Last Modified   : Wed 25 Feb 2015 10:26:06 AM CST
 #
 ##################################################
 
 function help(){
     echo -e  "help: \n \t john@doe$> rm_tmp_files /home/john/projects/ *.pyc"
-    kill -INT $$
+    exit_f
 }
 
 function rm_tmp_files(){
@@ -26,20 +24,19 @@ function rm_tmp_files(){
         type=$2
     elif [ $# -eq 1 ]; then
         echo -e "No directory provided. The current directory will be used"
-        dir='.'
+        dir=`pwd`
         type=$1
     elif [ $# -gt 2 ];then
         echo -e "Don't be a fool. There's no such thing as a third argument"
     else
         echo -e "A type in required though"
         help
-        kill -INT $$
+        exit_f
     fi
 
-    echo "find $dir -name $type | xargs ls -l"
-    find $dir -name $type | xargs ls -l
-
+    #echo "find $dir -name $type | xargs ls -l"
+    #find $dir -name $type | xargs ls -l
 }
 
-rm_tmp_files $1 $2
+#rm_tmp_files $1 $2
 
