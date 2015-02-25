b53eae228dcd8241284195920a1c6e8745aeafa0
josuebrunel <josuebrunel@gmail.com>
2015-02-25 10:01:19 -0600

    checking of the existence of a pub key OK

--- push_ssh_key.sh
+++ push_ssh_key.sh
@@ -4,14 +4,22 @@
 #   Filename        : push_ssh_key.sh
 #   Description     : Copy a ssh key to a remote host
 #   Creation Date   : 25-02-2015
-#   Last Modified   : Wed 25 Feb 2015 09:35:55 AM CST
+#   Last Modified   : Wed 25 Feb 2015 10:00:36 AM CST
 #
 ##################################################
 
 function push_ssh_key(){
+    pub_key="$HOME/.ssh/id_rsa.pub"
+    if [ ! -f $pub_key ]; then
+        echo -e "I think you should generate a pub_key first. \n \t -> ssh-keygen # if you have no private key \n \t -> ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub"
+        return 1
+    fi
+
     if [ ! -z $1 ]; then
         cat $HOME/.ssh/id_rsa.pub | ssh $1 'cat >> $HOME/.ssh/authorized_keys'
+        return 0
     else
         echo "You must provided a remote host i.e john@doe"
+        return 1
     fi
 }
