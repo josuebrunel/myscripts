8b12a4d8f54b45e851aa6d85671a62df4ca8f97b
josuebrunel <josuebrunel@gmail.com>
2015-02-25 09:42:18 -0600

    absoulte path added

--- push_ssh_key.sh
+++ push_ssh_key.sh
@@ -4,13 +4,13 @@
 #   Filename        : push_ssh_key.sh
 #   Description     : Copy a ssh key to a remote host
 #   Creation Date   : 25-02-2015
-#   Last Modified   : Wed 25 Feb 2015 07:16:52 AM CST
+#   Last Modified   : Wed 25 Feb 2015 09:35:55 AM CST
 #
 ##################################################
 
 function push_ssh_key(){
     if [ ! -z $1 ]; then
-        cat .ssh/id_rsa.pub | ssh $1 'cat >> .ssh/authorized_keys'
+        cat $HOME/.ssh/id_rsa.pub | ssh $1 'cat >> $HOME/.ssh/authorized_keys'
     else
         echo "You must provided a remote host i.e john@doe"
     fi
