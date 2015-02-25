786df07bae2f5ba7da8824424e5dc39fbb5edca3
josuebrunel <josuebrunel@gmail.com>
2015-02-25 09:43:03 -0600

    Logic has been improved

--- rm_tmp_files.sh
+++ rm_tmp_files.sh
@@ -1,33 +1,45 @@
+#!/bin/bash
+
 ##################################################
 #
 #   Author          : yosuke
 #   Filename        : rm_tmp_files.sh
 #   Description     : Remove ~, .pyc recursively 
 #   Creation Date   : 25-02-2015
-#   Last Modified   : Wed 25 Feb 2015 08:02:08 AM CST
+#   Last Modified   : Wed 25 Feb 2015 09:25:59 AM CST
 #
 ##################################################
 
-function rm_tmp_files(){
-    dir=$1
-    type=$2
+function help(){
+    echo -e  "help: \n \t john@doe$> rm_tmp_files /home/john/projects/ *.pyc"
+    kill -INT $$
+}
 
+function rm_tmp_files(){
+   
     if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-        echo -e  "help: \n \t john@doe$> rm_tmp_files /home/john/projects/ *.pyc"
+        help
+    fi
+
+    if [ $# -eq 2 ]; then
+        dir=$1
+        type=$2
+    elif [ $# -eq 1 ]; then
+        echo -e "No directory provided. The current directory will be used"
+        dir='.'
+        type=$1
+    elif [ $# -gt 2 ];then
+        echo -e "Don't be a fool. There's no such thing as a third argument"
+    else
+        echo -e "A type in required though"
+        help
         kill -INT $$
     fi
 
-    if [ -Z $dir ]; then
-        echo -e "No directory provided. The current directory will be used"
-        dir='.'
-    fi
-
-    if [ -Z $type ]; then
-        echo -e "No type provided. ~ will be used"
-        type='*~'
-    fi
-
-    find $dir -name $type | ls -l
+    echo "find $dir -name $type | xargs ls -l"
+    find $dir -name $type | xargs ls -l
 
 }
+
+rm_tmp_files $1 $2
 
