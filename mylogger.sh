c1caf343fdb715bcb9fe3ab2ecf039494434aa86
josuebrunel <josuebrunel@gmail.com>
2015-02-26 00:45:53 +0100

    _debug function OK

---
+++ mylogger.sh
@@ -0,0 +1,26 @@
+##################################################
+#
+#   Author          : josue
+#   Filename        : mylogger.sh
+#   Description     : custom logger for my shell scripts
+#   Creation Date   : 26-02-2015
+#   Last Modified   : Thu Feb 26 00:43:47 2015
+#
+##################################################
+
+function _log(){
+    priority=$1
+    message=$2
+    output=$3
+
+    if [ -z $output ]; then
+        logger -s -p "local7.${priority}" "${message}" 2>&1
+    else
+        logger -s -p "local7.${priority}" "${message}" 2>> $output 
+    fi
+
+}
+
+function _debug(){
+    _log "debug" $1 $2
+}
