fe8a1194d3b508313263d2110e641c0c906cb7b7
josuebrunel <josuebrunel@gmail.com>
2015-02-26 09:35:10 +0100

    fixt ntfs partition now using logger

--- fix_ntfs_partition.sh
+++ fix_ntfs_partition.sh
@@ -4,15 +4,17 @@
 #   Filename        : fix_ntfs_partition.sh
 #   Description     : fixes ntfs partition
 #   Creation Date   : 26-02-2015
-#   Last Modified   : Thu Feb 26 09:11:46 2015
+#   Last Modified   : Thu Feb 26 09:34:25 2015
 #
 ##################################################
 
 function fix_ntfs_partition(){
+    
+    LOG_OUTPUT=$HOME_SCRIPTS/logs/fix_ntfs_partition.log
 
     if [ -z $1 ]; then
-        echo -e "At least a partition must be provided"
-        #exit_f
+        echo -e 'At least a partition must be provided'
+        _info 'At least a partition must be provided'
         return 1
     fi    
 
