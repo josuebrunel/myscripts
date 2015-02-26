2703853617b543fa05eff46df5980922db788d2a
josuebrunel <josuebrunel@gmail.com>
2015-02-26 04:56:32 -0600

    push_ssh_key uses custom logger noew

--- push_ssh_key.sh
+++ push_ssh_key.sh
@@ -4,22 +4,30 @@
 #   Filename        : push_ssh_key.sh
 #   Description     : Copy a ssh key to a remote host
 #   Creation Date   : 25-02-2015
-#   Last Modified   : Wed 25 Feb 2015 10:11:51 AM CST
+#   Last Modified   : Thu 26 Feb 2015 04:55:01 AM CST
 #
 ##################################################
 
+export LOG_OUTPUT=$HOME_SCRIPTS/logs/push_ssh_key.log
+
 function push_ssh_key(){
+    _debug "START push_ssh_key"
     pub_key="$HOME/.ssh/id_rsa.pub"
     if [ ! -f $pub_key ]; then
         echo -e "I think you should generate a pub_key first. \n \t -> ssh-keygen # if you have no private key \n \t -> ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub"
-        exit_f
+        _error "No pub key found for user $USER"
+        return 1
     fi
 
     if [ ! -z $1 ]; then
-        cat $HOME/.ssh/id_rsa.pub | ssh $1 'cat >> $HOME/.ssh/authorized_keys'
-        return 0
+        if  ! cat $HOME/.ssh/id_rsa.pub | ssh $1 'cat >> $HOME/.ssh/authorized_keys' ; then
+            _error "${1} information aren't valid"
+        fi
     else
         echo "You must provided a remote host i.e john@doe"
-        exit_f
+        _info "You must provided a remote host i.e john@doe"
+        return 1
     fi
+    _debug "END push_ssh_key"
+    return 0
 }
