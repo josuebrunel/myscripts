afe85e2504a04cdb61c358b04ee61a483bdbf385
josuebrunel <josuebrunel@gmail.com>
2015-03-09 06:36:49 -0500

    main entrance point now

--- python/__init__.py
+++ python/__init__.py
@@ -4,7 +4,7 @@
 #   Filename        : __init__.py
 #   Description     : 
 #   Creation Date   : 09-03-2015
-#   Last Modified   : Mon 09 Mar 2015 06:02:14 AM CDT
+#   Last Modified   : Mon 09 Mar 2015 06:08:57 AM CDT
 #
 ##################################################
 
@@ -12,13 +12,34 @@ import os
 import imp
 
 def myimport(name, path):
-    """
+    """Handles module import according to a given path
     """
     home_scripts_python = os.path.join(os.environ['HOME_SCRIPTS'],'python')
     return imp.load_source(name,os.path.join(home_scripts_python,path))
 
-#pythonenv = myimport('pythonenv','pythonenv.py')
 db = myimport('db','db.py')
 
-#from pythonenv import *
-from db import *
+##AUTO COMPLETION
+import readline, rlcompleter
+if 'libedit' in readline.__doc__:
+    readline.parse_and_bind("bind ^I rl_complete")
+else:
+    readline.parse_and_bind("tab: complete")
+
+#USEFUL METHOD
+def get_real_path(f):
+    """Retuns the realpath of a file
+    """
+    return os.path.realpath(f)
+    
+def get_joined_path(a, b):
+    """Returns joined path of to file
+    """
+    return os.path.join(a,b)
+
+def get_file_content(filename):
+    """Returns content of a given file
+    """
+    with open(filename) as f :
+        return f.read()
+
