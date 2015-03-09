e55d2883e29d0af1a8e878632ba33685d643579f
josuebrunel <josuebrunel@gmail.com>
2015-03-09 09:32:33 +0100

    remove db part

--- python/pythonenv.py
+++ python/pythonenv.py
@@ -4,6 +4,8 @@ import datetime
 import logging
 import sqlite3
 
+from db import *
+
 ##AUTO COMPLETION
 import readline, rlcompleter
 if 'libedit' in readline.__doc__:
@@ -28,78 +30,3 @@ def get_file_content(filename):
     with open(filename) as f :
         return f.read()
 
-##GENERIC DATABASE  CLASS
-
-class Database(object):
-    """Just an usual class
-    """
-    __metaclass__ = abc.ABCMeta
-
-    def __init__(self,):
-        """
-        """
-        self.con = None
-        self.cr = None
-
-    @abc.abstractmethod
-    def connect(self,):
-        """Connects to the db
-        """
-        return None
-
-    def create_table(self, *args, **kwargs):
-        """Creates a table
-        """
-        query = "CREATE TABLE {} ".format(args[0])
-        print query
-
-    def executeQuery(self, query):
-        """Executes a SQL query
-        """
-        try:
-            self.cr.execute(query)
-            return self.cr
-        except Exception, e:
-            print(e)
-            
-        return False
-
-    def commit(self,):
-        """Commits the  last change
-        """
-        try:
-            self.con.commit()
-            self.last_id()
-        except Exception, e:
-            print(e)
-
-    def last_id(self,):
-        """Returns the id of the last affected row
-        """
-        return self.cr.rowcount
-        
-## SQLITE CLASS
-
-class MySqlite(Database):
-    """Simple SQlite3 class handler
-    """
-
-    def __init__(self, dbfile):
-        """
-        """
-        super(MySqlite, self).__init__()
-        
-        self.dbfile = dbfile
-
-    def connect(self,):
-        """Connects to the db
-        """
-        try:
-            self.con = sqlite3.connect(self.dbfile)
-            self.cr = self.con.cursor()
-            return self.con
-        except Exception, e:
-            print(e)
-
-        return None
-        
