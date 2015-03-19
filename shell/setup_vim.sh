050c680c270d06e4852df7a4d0b62f1b5426f05e
josuebrunel <josuebrunel@gmail.com>
2015-03-19 16:41:29 -0500

    fix #4: script for vim install OK

---
+++ shell/setup_vim.sh
@@ -0,0 +1,26 @@
+##################################################
+#
+#   Author          : yosuke
+#   Filename        : setup_vim.sh
+#   Description     : setup vim config
+#   Creation Date   : 19-03-2015
+#   Last Modified   : Thu 19 Mar 2015 04:33:29 PM CDT
+#
+##################################################
+
+function setup_vim(){
+    LOG_OUTPUT=$HOME_SCRIPTS/logs/setup_vim.log
+    REPOSITORY=https://github.com/josuebrunel/myvim.git
+
+    _debug "===START==="
+
+    cd ~ # go to home directory
+
+    _debug "Cloning ${REPOSITORY} into .vim/ "
+    git clone $REPOSITORY .vim
+
+    _debug "Installing vim"
+    ln -s .vim/.vimrc .vimrc
+
+    _debug "===END==="
+}
