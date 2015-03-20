11e530dd78e78cb5facf9f9575945b8ee49ff604
josuebrunel <josuebrunel@gmail.com>
2015-03-20 08:17:57 -0500

    close #5 : Editor config done

--- shell/setup_editors.sh
+++ shell/setup_editors.sh
@@ -4,31 +4,32 @@
 #   Filename        : setup_vim.sh
 #   Description     : setup vim config
 #   Creation Date   : 19-03-2015
-#   Last Modified   : Fri 20 Mar 2015 08:11:44 AM CDT
+#   Last Modified   : Fri 20 Mar 2015 08:16:53 AM CDT
 #
 ##################################################
 
 function setup_editor(){
 
     today=$(date +"%d-%m-%Y")
-    #LOG_OUTPUT=$HOME_SCRIPTS/logs/setup_vim-$today.log
+    LOG_OUTPUT=$HOME_SCRIPTS/logs/setup_vim-$today.log
 
     if [ -z $1 ]; then
         echo -e "An editor name is required i.e vim or emacs"
         _info "Editor name not provided"
         return 1
     fi
-    echo -e $1
+
     if [ $1 != "emacs" ] && [ $1 != "vim" ]; then
         echo -e "Only emacs and vim are accepted as editor name"
         _error "Invalid editor name : ${1}"
         return 1
     fi
+
     _debug "Valid editor name : $1" 
     REPOSITORY=https://github.com/josuebrunel/my$1.git
-    editor="$1"
+    editor=$1
 
-    if [ $editor == "emacs"]; then
+    if [ $editor == 'emacs' ]; then
         dir='.emacs.d/'
         file='.emacs'
     else
@@ -43,10 +44,10 @@ function setup_editor(){
     cd $HOME # go to home directory
 
     _debug "Cloning ${REPOSITORY} into ${dir} "
-    #git clone $REPOSITORY $dir
+    git clone $REPOSITORY $dir
 
-    _debug "Installing vim"
-    #ln -s "${dir}/{$vim}" $file
+    _debug "Installing ${editor} config"
+    ln -s "${dir}/{$vim}" $file
     cd -
     _debug "===END==="
 }
