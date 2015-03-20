cc5cb44ad8010baa2e7deea116365cce54cc251b
josuebrunel <josuebrunel@gmail.com>
2015-03-20 07:58:27 -0500

    #5 : fix loading in .profile and erros in script

--- shell/setup_editors.sh
+++ shell/setup_editors.sh
@@ -4,19 +4,22 @@
 #   Filename        : setup_vim.sh
 #   Description     : setup vim config
 #   Creation Date   : 19-03-2015
-#   Last Modified   : Fri 20 Mar 2015 06:02:26 AM CDT
+#   Last Modified   : Fri 20 Mar 2015 07:57:35 AM CDT
 #
 ##################################################
 
-function setup_vim(){
-    today=$(now +'%d-%m-%Y')
+function setup_editor(){
+
+    today=$(date +"%d-%m-%Y")
     LOG_OUTPUT=$HOME_SCRIPTS/logs/setup_vim-$today.log
 
-    if [-z $1 ]; then
+    if [ -z $1 ]; then
         echo -e "An editor name is required i.e vim or emacs"
         _info "Editor name not provided"
         return 1
-    elif [ $1 != "emacs" ] || [ $1 != "vim" ]
+    fi
+
+    if [ $1 != "emacs" ] || [ $1 != "vim" ]; then
         echo -e "Only emacs and vim are accepted as editor name"
         _error "Invalid editor name : ${1}"
         return 1
@@ -39,10 +42,10 @@ function setup_vim(){
     cd ~ # go to home directory
 
     _debug "Cloning ${REPOSITORY} into ${dir} "
-    git clone $REPOSITORY $dir
+    #git clone $REPOSITORY $dir
 
     _debug "Installing vim"
-    ln -s "${dir}/{$vim}" $file
+    #ln -s "${dir}/{$vim}" $file
 
     _debug "===END==="
 }
