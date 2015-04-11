88525a457435dc0b2fd87799ed222ee2817dac3f
josuebrunel <josuebrunel@gmail.com>
2015-04-11 06:01:01 +0200

    fix #8: :smile:

--- shell/ssh_connect_to.sh
+++ shell/ssh_connect_to.sh
@@ -4,18 +4,19 @@
 #   Filename        : ssh_connect_to.sh
 #   Description     :
 #   Creation Date   : 11-04-2015
-#   Last Modified   : Sat Apr 11 05:49:15 2015
+#   Last Modified   : Sat Apr 11 05:58:16 2015
 #
 ##################################################
 
 
 function ssh-connect-to(){
     host=$1
-
+    unset r_host
     while read -r line || [[ -n $line ]]; do
-        r_hostname=`echo $line | cut -f 1 -d ' '`
-        r_host=`echo $line | cut -f 2 -d ' '`
-        if [[ $host == $r_hostname ]]; then
+       
+        if [[ $host == `echo $line | cut -f 1 -d ' '` ]]; then
+            r_host=`echo $line | cut -f 2 -d ' '`
+            _info "Macth found"
             break
         fi       
     done < "$HOME_SCRIPTS/cfg/ssh.txt"
