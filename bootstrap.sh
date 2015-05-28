78a78b1f0f92ad43eab5a305b0f03129e236b389
josuebrunel <josuebrunel@gmail.com>
2015-05-28 07:40:59 +0200

    better handling of path and gitconfig setup added

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,15 +4,24 @@
 #   Filename        : bootstrap.sh
 #   Description     : env installer
 #   Creation Date   : 13-05-2015
-#   Last Modified   : Wed 13 May 2015 02:05:39 PM CEST
+#   Last Modified   : Thu 28 May 2015 07:40:15 AM CEST
 #
 ##################################################
 set -x
 cd ~
-git clone https://github.com/josuebrunel/myscripts.git .scripts
+git clone https://github.com/josuebrunel/myscripts.git ~/.scripts
+
+# if .profile already exists
 if [ -f ~/.profile ];then
     mv ~/.profile ~/.profile.bak
 fi
-ln -s .scripts/profile .profile
+ln -s ~/.scripts/profile ~/.profile
+
+# if .gitconfig already exists
+if [ -f ~/.profile ];then
+    mv ~/.gitconfig ~/.gitconfig.bak
+fi
+ln -s ~/.scripts/gitconfig ~/.gitconfig 
+
 source .profile
 
