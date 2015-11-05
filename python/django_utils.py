6fca3bf48a1b851ed6f4ebf2f3ee38ae9ac63635
Josue Kouka <josuebrunel@gmail.com>
2015-11-05 16:58:28 +0100

    loading authentic2 salad

--- python/django_utils.py
+++ python/django_utils.py
@@ -4,7 +4,7 @@
 #   Filename        : django.py
 #   Description     :
 #   Creation Date   : 21-10-2015
-#   Last Modified   : Wed 21 Oct 2015 02:32:48 PM CEST
+#   Last Modified   : Thu 05 Nov 2015 04:48:12 PM CET
 #
 ##################################################
 
@@ -20,3 +20,14 @@ if os.environ.get('DJANGO_SETTINGS_MODULE', None):
     User = get_user_model()
 
     users = User.objects.all()
+
+    if os.environ.get('DJANGO_SETTINGS_MODULE') == 'authentic2.settings':
+        from authentic2.models import Service
+        from django_rbac.utils import get_role_model
+        Role = get_role_model()
+
+        services = Service.objects.all()
+        roles = Role.objects.all()
+
+
+
